b2380a67ddcccf7e8f14af6d8e44a8d0d78c7681
McDaniells <theluminary29@gmail.com>
2025-12-18 02:34:15 +0100

    Menu items clickable from 110%+ zoom

--- frontend-v2/src/components/layout/header.jsx
+++ frontend-v2/src/components/layout/header.jsx
@@ -196,13 +196,14 @@ const Header = React.memo(function Header() {
       <div
         className={`${
           mobileNavOpen ? 'block' : 'hidden'
-        } fixed top-0 left-0 bottom-0 w-4/6 sm:max-w-xs z-50 pointer-events-auto`}
+        } fixed top-0 left-0 bottom-0 right-auto z-50 pointer-events-auto`}
+        style={{ width: 'min(100vw, calc(100% - 2rem))' }}
       >
         <div
           className="fixed inset-0 z-0 bg-black opacity-60 pointer-events-auto"
           onClick={toggleMobileNav}
         ></div>
-        <nav className="relative z-10 px-9 pt-8 h-full bg-black overflow-y-auto pointer-events-auto">
+        <nav className="relative z-10 w-full px-9 pt-8 h-full bg-black overflow-y-auto pointer-events-auto">
           <div className="flex flex-wrap justify-between h-full">
             <div className="w-full">
               <div className="flex items-center justify-between -m-2">
